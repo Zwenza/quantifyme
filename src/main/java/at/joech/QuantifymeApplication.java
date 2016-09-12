b5c0ce4ceee12e0ca08c00ab8d43e3beeb63fa62
David Jöch <david.joech@catalysts.cc>
2016-09-12 19:25:44 +0200

    set-up sprint boot project

---
+++ src/main/java/at/joech/QuantifymeApplication.java
@@ -0,0 +1,12 @@
+package at.joech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuantifymeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuantifymeApplication.class, args);
+	}
+}
